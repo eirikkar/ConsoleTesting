a8c3eda865f2816420cf0a567fcf67b319408830
eirikkar <eirikkar@proton.me>
2024-11-22 11:12:46 +0100

    Add View class with ViewMainMenu method

---
+++ Classes/View.cs
@@ -0,0 +1,13 @@
+public class View
+{
+    public void ViewMainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Test menu");
+        Console.WriteLine("1. Test 1");
+        Console.WriteLine("2. Test 2");
+        Console.WriteLine("3. Test 3");
+        Console.WriteLine("4. Test 4");
+        Console.WriteLine("5. Exit");
+    }
+}
